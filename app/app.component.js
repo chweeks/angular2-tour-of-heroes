03af1e7432eae29578d32b206a62c453c3f2cea2
chweeks <chrisharryweeks@gmail.com>
2016-02-02 21:48:20 +0000

    added hero interface appComponent properties

--- app/app.component.js
+++ app/app.component.js
@@ -18,11 +18,18 @@ System.register(['angular2/core'], function(exports_1) {
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
+                    this.title = 'Tour of Heroes';
+                    this.hero = {
+                        id: 1,
+                        name: 'Windstorm'
+                    };
                 }
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: '<h1>My First Angular 2 App</h1>'
+                        template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>\
+             <div><label>id: </label>{{hero.id}}</div>\
+             <div><label>name: </label>{{hero.name}}</div>'
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
